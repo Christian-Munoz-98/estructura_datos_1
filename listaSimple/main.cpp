2250e8c5b8dde4ea370507e74945178720f3d719
Muñoz Christian <79419191+Christian-Munoz-98@users.noreply.github.com>
2022-02-09 14:52:00 -0600

    Main

--- listaSimple/main.cpp
+++ listaSimple/main.cpp
@@ -6,45 +6,45 @@ using namespace std;
 int main()
 {
     // NULL
-    LinkedList *linkedList = new LinkedList();
+    ListaEnlazada *linkedList = new ListaEnlazada();
 
     // 43->NULL
-    linkedList->InsertHead(43);
+    linkedList->InsertarInicio(43);
 
     // 76->43->NULL
-    linkedList->InsertHead(76);
+    linkedList->InsertarInicio(76);
 
     // 76->43->15->NULL
-    linkedList->InsertTail(15);
+    linkedList->InsertarFinal(15);
 
     // 76->43->15->44->NULL
-    linkedList->InsertTail(44);
+    linkedList->InsertarFinal(44);
 
     // Print the list element
     cout << "First Printed:" << endl;
-    linkedList->PrintList();
+    linkedList->ImprimirLista();
     cout << endl;
 
     // 76->43->15->44->100->NULL
-    linkedList->Insert(4, 100);
+    linkedList->Insertar(4, 100);
 
     // 76->43->15->48->44->100->NULL
-    linkedList->Insert(3, 48);
+    linkedList->Insertar(3, 48);
 
     // 22->76->43->15->48->44->100->NULL
-    linkedList->Insert(0, 22);
+    linkedList->Insertar(0, 22);
 
     // Print the list element
     cout << "Second Printed:" << endl;
-    linkedList->PrintList();
+    linkedList->ImprimirLista();
     cout << endl;
 
     // Get value of the second index
     // It should be 43
     cout << "Get value of the second index:" << endl;
-    Node * get = linkedList->Get(2);
+    Nodo * get = linkedList->Get(2);
     if(get != NULL)
-        cout << get->Value;
+        cout << get->Valor;
     else
         cout << "not found";
     cout << endl << endl;
@@ -52,29 +52,29 @@ int main()
     // Find the position of value 15
     // It must be 3
     cout << "The position of value 15:" << endl;
-    int srch = linkedList->Search(15);
+    int srch = linkedList->Buscar(15);
     cout << srch << endl << endl;
 
     // Remove first element
     cout << "Remove the first element:" << endl;
-    linkedList->Remove(0);
+    linkedList->Remover(0);
     // 76->43->15->48->44->100->NULL
-    linkedList->PrintList();
+    linkedList->ImprimirLista();
     cout << endl;
 
     // Remove fifth element
     cout << "Remove the fifth element:" << endl;
-    linkedList->Remove(4);
+    linkedList->Remover(4);
     // 76->43->15->48->100->NULL
-    linkedList->PrintList();
+    linkedList->ImprimirLista();
     cout << endl;
 
     // Remove tenth element
     cout << "Remove the tenth element:" << endl;
-    linkedList->Remove(9);
+    linkedList->Remover(9);
     // Nothing happen
     // 76->43->15->48->100->NULL
-    linkedList->PrintList();
+    linkedList->ImprimirLista();
     cout << endl;
 
     return 0;
