efa508f627298d3da8c905f91a49501a235f6292
Christian Muñoz <chgeovany.15@gmail.com>
2022-03-03 18:46:19 -0600

    Falta manejo de errores

--- Proyectos/02-listaDoble/main.cpp
+++ Proyectos/02-listaDoble/main.cpp
@@ -9,158 +9,136 @@
 */
 
 using namespace std;
-
-int main()
-{
-    ListaDoble* lista = new ListaDoble();
-
-    lista->InsertarInicio();
-
-    lista->InsertarInicio();
-
-    lista->InsertarFinal();
-
-    lista->InsertarFinal();
-
-    cout << "Mostrar:" << endl;
-    lista->MostrarLista();
-    cout << endl;
-
-    lista->InsertarMedio(4);
-
-    lista->InsertarMedio(3);
-
-    lista->InsertarMedio(0);
-
-    cout << "Mostrar Reversa:" << endl;
-    lista->MostrarReversa();
-    cout << endl;
-
-    return 0;
-}
-
-/*
-#include <iostream>
-
-using namespace std;
-
 void opciones(int);
+ListaDoble* lista = new ListaDoble();
 
 int main(){
     int opc;
     do{
         system("cls");
-        cout<<"1)Opcion 1"<<endl
-        <<"2)Opcion 2" <<endl
-        <<"3)Opcion 3"<<endl
-        <<"4)Opcion 4"<<endl
-        <<"5)Opcion 5"<<endl
-        <<"6)Opcion 6"<<endl
-        <<"7)Opcion 7"<<endl
-        <<"8)Opcion 8"<<endl
-        <<"9)Opcion 9"<<endl
-        <<"10)Opcion 10"<<endl
-        <<"11)Opcion 11"<<endl
-        <<"12)Opcion 12"<<endl
-        <<"13)Salir"<<endl
+        cout<<"1)Insertar"<<endl
+        <<"2)Eliminar" <<endl
+        <<"3)Buscar"<<endl
+        <<"4)Primero"<<endl
+        <<"5)Ultimo"<<endl
+        <<"6)Tamanho"<<endl
+        <<"7)Vaciar"<<endl
+        <<"8)Mostrar Lista"<<endl
+        <<"9)Salir"<<endl
         <<"Elija una opcion >>>>>";
         cin>>opc;
         opciones(opc);
-    }while(opc!=13);
+    }while(opc!=9);
     return 0;
 }
 
 void opciones(int opc){
+    char insert;
+    int index;
+    //bool confirmation;
+    string animal;
+    NodoDoble* NodoEncontrado;
     switch (opc){
     case 1:
-
         system("cls");
-        system("pause");
+        cout << "INSERTAR ELEMENTO" << endl << endl;
+        cout <<"a)Insertar al inicio"<< endl
+        <<"b)Insertar al final"<< endl
+        <<"c)Insertar en indice intermedio"<< endl
+        <<"Elija una opcion>>>";
+        cin>>insert;
+        switch (insert){
+            case 'a':
+                lista->InsertarInicio();
+                break;
+            case 'b':
+                lista->InsertarFinal();
+                break;
+            case 'c':
+                cout<<"ELija un indice>>";
+                cin>>index;
+                lista->InsertarMedio(index);
+                break;
+            default:
+                cout<<"Opcion invalida"<<endl;
+                break;
+        }
         break;
 
     case 2:
-
         system("cls");
+        cout << "ELIMINAR ELEMENTO" << endl << endl;
+        cout << "Ingrese el nombre del animal a eliminar>>>";
+        cin >> animal;
+        lista->RemoverMedio(lista->BuscarIndice(animal));
+        cout << "Elemento removido con exito" << endl;
         system("pause");
         break;
 
     case 3:
-
         system("cls");
+        cout << "BUSCAR ELEMENTO" << endl << endl;
+        cout << "Inngrese el animal a buscar>>>";
+        cin >> animal;
+        NodoEncontrado = lista->Buscar(lista->BuscarIndice(animal));
+        NodoEncontrado->a.mostrar();
         system("pause");
         break;
     
     case 4:
-
         system("cls");
+        cout << "MOSTRAR EL PRIMER ELEMENTO" << endl << endl;
+        if(lista->Cabecera==NULL){
+            cout << "La lista esta vacia" << endl;
+        }
+        else{
+            lista->Primero()->a.mostrar();
+        }
         system("pause");
         break;
 
     case 5:
-
         system("cls");
+        cout << "MOSTRAR EL PRIMER ELEMENTO" << endl << endl;
+        if(lista->Cabecera==NULL){
+            cout << "La lista esta vacia" << endl;
+        }
+        else{
+            lista->Ultimo()->a.mostrar();
+        }
         system("pause");
         break;
         
 
     case 6:
-
         system("cls");
+        cout << "MOSTRAR TAMANO DE LA LISTA" << endl << endl;
+        cout << "La lista tiene " << lista->Tamanho() << " elementos" << endl;
         system("pause");
         break;
 
     case 7:
-
         system("cls");
+        cout << "VACIAR LA LISTA" << endl << endl;
+        lista->VaciarLista();
         system("pause");
         break;
 
     case 8:
-
         system("cls");
+        cout << "MOSTRAR LISTA" << endl << endl;
+        if (lista->Cabecera==NULL){
+            cout << "La lista esta vacia" << endl;
+        }
+        else{
+            lista->MostrarLista();
+        }
         system("pause");
         break;
 
     case 9:
-
         system("cls");
-        system("pause");
-        break;
-
-    case 10:
-    
-        system("cls");
-        system("pause");
-        break;
-
-    case 11:
-    
-        system("cls");
-        system("pause");
-        break;
-
-    case 12:
-    
-        system("cls");
-        system("pause");
-        break;
-
-    case 13:
-    
-        system("cls");
-        system("pause");
-        break;
-
-    case 14:
-    
-        system("cls");
-        system("pause");
-        break;
-
-    case 15:
-    
-        system("cls");
-        cout << "Saliendo.." << endl;
+        cout<<"Saliendo..."<<endl;
         system("pause");
         break;
 
@@ -171,4 +149,3 @@ void opciones(int opc){
         break;
     }
 }
-*/
